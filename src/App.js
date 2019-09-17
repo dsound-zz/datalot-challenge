588618cfcf71cab26959139eb8d30621b94ffc96
dsound-zz <demiansims@gmail.com>
2019-09-17 18:50:36 -0400

    vanilla-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Container, Modal, ModalBody, ModalFooter, Jumbotron, Row, Col, Card, CardTitle, CardBody, CardText, Form, FormGroup, Label, Button, Input  } from 'reactstrap';
 import carimg from './images/cars.jpg'
 import './App.css';
 
-const ageMenu = ["]
 class App extends React.Component {
   state = {
-    openModal: false,
+    showModal: false,
     focusAfterClose: true,
     modalMessage: "",
     zipcode: "",
@@ -23,32 +21,66 @@ class App extends React.Component {
     e.preventDefault();
     console.log(zipcode, currentlyInsured, ageRange);
     if (currentlyInsured === "no") {
-      this.setState({ openModal: true, modalMessage: "Sorry, no rates available" });
+      this.setState({ showModal: !this.state.showModal, modalMessage: "Sorry, no rates available" });
       return;
     }
     if (ageRange === "over") {
       this.setState({
-        openModal: true,
+        showModal: this.state.showModal,
         modalMessage: "Yes, we have great rates available!"
       });
 
     } else if (ageRange === "under") {
-      this.setState({ openModal: true, modalMessage: "Coming soon!" })
+      this.setState({ showModal: this.state.showModal, modalMessage: "Coming soon!" })
     }
     this.setState({ zipcode: "", currentlyInsured: "", ageRange: "" });
   };
 
   toggle = () => {
-    this.setState(({ openModal }) => ({ openModal: !openModal }));
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
   }
+
+   ModalMessage = () => (
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+  )
+
 
   render() {
     console.log(this.state);
     const { zipcode, currentlyInsured, ageRange } = this.state;
     return (
       <>
-        <Container className="container-1">
-          <Jumbotron className="jumbotron">
+        <div className="container-1">
+          <div className="jumbotron">
             <div className="header-text">
               <h1 style={{ color: "rgb(255, 255, 255)" }}>
                 Lower Your Auto
@@ -62,28 +94,31 @@ class App extends React.Component {
 
             {/* Create Card  */}
 
-            <Card className="card">
-              <CardBody>
-                <CardTitle tag="h4" style={{ fontStye: "strong" }}>
+            <div className="card">
+              <div className="card-body">
+                <div
+                  className="card-title"
+                  tag="h4"
+                  style={{ fontStye: "strong" }}
+                >
                   Cheap Auto Coverage
-                </CardTitle>
-                <CardText>
+                </div>
+                <div className="card-title">
                   Complete just <strong>3 simple steps</strong> to
                   <br />
                   get quotes from top providers in your area.
-                </CardText>
+                </div>
 
                 {/* Start Form content in card here  */}
 
-                <Form
+                <form
                   className="form"
                   onSubmit={e =>
                     this.handleSubmit(zipcode, currentlyInsured, ageRange, e)
                   }
                 >
-                  <FormGroup>
-                    <Label for="exampleEmail"></Label>
-                    <Input
+                  <div className="form-group">
+                    <input
                       onChange={this.handleChange}
                       value={this.state.zipcode}
                       type="text"
@@ -92,23 +127,25 @@ class App extends React.Component {
                       placeholder="zipcode"
                       required
                     />
-                    <Label for="currentlyInsured"></Label>
-                    <Input
+
+                    <select
+                      className="form-control"
                       onChange={this.handleChange}
                       value={this.state.currentlyInsured}
-                      type="select"
                       name="currentlyInsured"
                       id="currentlyInsured"
+                      placeholder="currently insured?"
                       required
                     >
-                      <option value="" selected disabled>
-                        Currently Insured?
+                      <option value="" disabled>
+                        Already insured?
                       </option>
                       <option value="yes">Yes</option>
                       <option value="no">No</option>
-                    </Input>
-                    <Label for="ageRange"></Label>
-                    <Input
+                    </select>
+
+                    <select
+                      className="form-control"
                       onChange={this.handleChange}
                       value={this.state.ageRange}
                       type="select"
@@ -116,75 +153,65 @@ class App extends React.Component {
                       id="ageRange"
                       required
                     >
-                      <option value="" selected disabled>
-                        Your Age Range
+                      <option value="" disabled>
+                        Age Range
                       </option>
                       <option value="under">Under 25</option>
                       <option value="under">25 to 34</option>
                       <option value="over">35 to 44</option>
                       <option value="over">45 to 55</option>
                       <option value="over">over 55</option>
-                    </Input>
-                  </FormGroup>
-                 
-                    <Button
-                      className="btn"
-                      type="submit"
-                      color="danger"
-                      size="lg"
-                    >
-                      <h5>Find Lower Rates</h5>
-                    </Button>
-                    <p class="secure-text">
-                      <small>Your information is safe and secure.</small>
-                    </p>
-                </Form>
+                    </select>
+                  </div>
+
+                  <button
+                    className="btn btn-danger"
+                    onClick={this.state.toggle}
+                    type="submit"
+                    size="lg"
+                  >
+                    <h5>Find Lower Rates</h5>
+                  </button>
+    
+                  <p className="secure-text">
+                    <small>Your information is safe and secure.</small>
+                  </p>
+                </form>
 
                 {/* End form here  */}
-              </CardBody>
-            </Card>
-          </Jumbotron>
-        </Container>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* Body of page  */}
 
-        <Container className="container-2">
-          <Row>
-            <Col lg="6">
+        <div className="container-2">
+          <div className="row">
+            <div className="col-6">
               <h3>
                 We provide free quotes from top providers in your area in less
                 time.
               </h3>
               <div className="list">
-              <ul>
-                <li>Save time researching policies.</li>
-                <li>Get a fast response with no waiting.</li>
-                <li>Compare low-cost policies.</li>
-                <li>Save up to 30% on auto insurance.</li>
-              </ul>
+                <ul>
+                  <li>Save time researching policies.</li>
+                  <li>Get a fast response with no waiting.</li>
+                  <li>Compare low-cost policies.</li>
+                  <li>Save up to 30% on auto insurance.</li>
+                </ul>
               </div>
-            </Col>
-            <Col lg="6">
-            <div className="car-photo"><img src={carimg} alt="cars" /></div>
-            
-            </Col>
-          </Row>
-        </Container>
+            </div>
+            <div className="col-6">
+              <div className="car-photo">
+                <img src={carimg} alt="cars" />
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* Create Modal  */}
-
-        <Modal style={{ width: "30em", height: "15em"}}
-          returnFocusAfterClose={this.state.focusAfterClose}
-          isOpen={this.state.openModal}
-        >
-          <ModalBody style={{ background: "rgb(12, 212, 184)" }}>
-            <h4 style={{ textAlign: "center"}}>{this.state.modalMessage}</h4>
-          </ModalBody>
-
-          <Button color="primary" onClick={this.toggle}>
-            Close
-          </Button>
-        </Modal>
+      
       </>
     );
   }
